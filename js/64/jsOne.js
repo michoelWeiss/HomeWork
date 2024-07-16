1e475bff139a7976582e8f0560c69258f11bd22b
michoelWeiss <michoelweissmw@gmail.com>
2024-07-16 14:12:50 -0400

    homework 64

---
+++ js/64/jsOne.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const myName = "michoel weiss";
+const myEmail = "michoelweissmw@gmail.com";
---
+++ js/64/jsTwo.js
@@ -0,0 +1,15 @@
+'use strict'
+
+console.log(myName, typeof myName);
+console.log(myEmail, typeof myEmail);
+const password = prompt("What is your password?");
+alert(password);
+
+const userName = prompt("whats your name");
+const userEmail = prompt("whats your email");
+
+alert("Name: " + userName + " Email: " + userEmail);
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+  }
