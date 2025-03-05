2041ed3472910d0866a40bc6ad7490562f8c0d02
michoelWeiss <michoelweissmw@gmail.com>
2025-03-05 15:40:22 -0500

    hw_112

--- js/99/my-first-async-io.js
+++ js/99/my-first-async-io.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-fs.readFile(process.argv[2], (err, data) =>{
-    if(err)
+fs.readFile(process.argv[2], (err, data) => {
+    if (err)
         console.log('there was an error');
     const file = data.toString();
     const lines = file.split('\n').length - 1;
